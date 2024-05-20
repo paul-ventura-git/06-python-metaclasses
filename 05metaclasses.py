70d79bb18c780e44666ea1821e06697b6fbfd6c1
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-20 14:48:34 -0500

    metaclasses, edited

--- 05metaclasses.py
+++ 05metaclasses.py
@@ -1,8 +1,8 @@
-class MyMeta(type):
+class MyMeta(type): # When it inherits from "type" it is a metaclass
     def __call__(self, *args, **kwargs):
         print(f'{self.__name__} is called'
-              f' with args={args}, kwargs={kwarg}')
-
+              f' with args={args}, kwargs={kwargs}')
+    # implicitly it returns NONE (of a NoneType)
 class Parent(metaclass=MyMeta):
     def __new__(cls, name, age):
         print('new is called')
@@ -15,5 +15,5 @@ class Parent(metaclass=MyMeta):
 
 parent = Parent('John', 35)
 # Parent is called with args=('John', 35), kwargs={}
-type(parent)
+print(type(parent))
 # NoneType
